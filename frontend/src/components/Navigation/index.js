33e816079a56ba8483a9ad668135725f1796ff23
Barry Mattern <bdmattern@gmail.com>
2021-01-01 23:14:43 -0700

    - Update CSS on HomePage - Create visual theme of site

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -23,7 +23,7 @@ function Navigation({ isLoaded }){
   }
 
   return (
-    <ul>
+    <ul className='navigation'>
       <li>
         <NavLink exact to="/">Home</NavLink>
         {isLoaded && sessionLinks}
